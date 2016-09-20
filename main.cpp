a9d7f20d65d973254a6791ec2d219955117e0dac
Alex Carstensen <matthacker.alex@gmail.com>
2016-09-20 15:44:58 +0200

    added color to the window with render and texture

--- main.cpp
+++ main.cpp
@@ -23,15 +23,45 @@ int main()
             SCREEN_WIDTH,
             SCREEN_HEIGHT,
             SDL_WINDOW_SHOWN);
+
     if (window == NULL)
     {
         SDL_Quit();
         return 1;
     }
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == NULL)
+    {
+        cout << "Could not create renderer" << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    if(texture == NULL)
+    {
+        cout << "Could not create renderer" << endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Uint32* buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+    //setting the color
+    memset(buffer, 0xFF, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+
     bool quit = false;
     SDL_Event event;
-    
+
     while(!quit)
     {
         while(SDL_PollEvent(&event))
@@ -43,6 +73,10 @@ int main()
         }
     };
 
+    delete [] buffer;
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
