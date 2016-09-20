7ab0393927567412bcd911a6d309c4cf073d559d
Alex Carstensen <matthacker.alex@gmail.com>
2016-09-20 15:20:30 +0200

    added a window in main

--- main.cpp
+++ main.cpp
@@ -4,15 +4,48 @@
 using namespace std;
 int main()
 {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT= 600;
+
+
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         cout << "SDL init failed." << endl;
-        return -1;
-
+        return 1;
     }
 
-    std::cout << "SDL init succeeded" << std::endl;
+    cout << "SDL init succeeded" << endl;
 
+    SDL_Window* window = SDL_CreateWindow(
+            "Particle Fire Explosion",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+    
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+    };
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
+
     return 0;
 }
